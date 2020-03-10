0380d1bb18c1e534541a8ae4531c1e4fa6f171b8
phoenik <phoenik@gmail.com>
2020-03-09 22:54:20 -0700

    Bersker JP effect

--- scripts/globals/effects/berserk.lua
+++ scripts/globals/effects/berserk.lua
@@ -8,8 +8,10 @@ require("scripts/globals/status")
 
 function onEffectGain(target,effect)
     local power = effect:getPower()
-    target:addMod(tpz.mod.ATTP, power)
-    target:addMod(tpz.mod.RATTP, power)
+    local jp_value = target:getJobPointValue(dsp.jp.BERSERK_EFFECT)
+    local jp_effect = jp_value * 2
+    target:addMod(tpz.mod.ATTP, power + jp_effect)
+    target:addMod(tpz.mod.RATTP, power + jp_effect)
     target:addMod(tpz.mod.DEFP, -power)
 end
 
@@ -18,7 +20,9 @@ end
 
 function onEffectLose(target,effect)
     local power = effect:getPower()
-    target:delMod(tpz.mod.ATTP, power)
-    target:delMod(tpz.mod.RATTP, power)
+    local jp_value = target:getJobPointValue(dsp.jp.BERSERK_EFFECT)
+    local jp_effect = jp_value * 2
+    target:delMod(tpz.mod.ATTP, power + jp_effect)
+    target:delMod(tpz.mod.RATTP, power + jp_effect)
     target:delMod(tpz.mod.DEFP, -power)
 end
