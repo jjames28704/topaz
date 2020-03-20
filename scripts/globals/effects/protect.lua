baeef1f7a95ac580835216924492f79adaced81f
dynisor <dynisor@gmail.com>
2020-03-20 10:05:54 -0700

    Adding in missing PLD gift effects

--- scripts/globals/effects/protect.lua
+++ scripts/globals/effects/protect.lua
@@ -7,12 +7,22 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target,effect)
-   target:addMod(tpz.mod.DEF,effect:getPower())
+   local power = effect:getPower();
+   if target:hasMod(tpz.mod.PROTECT_EFFECT) then
+      power = power * (target:getMod(tpz.mod.PROTECT_EFFECT) / 100)
+  end
+
+   target:addMod(tpz.mod.DEF,power)
 end
 
 function onEffectTick(target,effect)
 end
 
 function onEffectLose(target,effect)
-   target:delMod(tpz.mod.DEF,effect:getPower())
+   local power = effect:getPower();
+   if target:hasMod(tpz.mod.PROTECT_EFFECT) then
+      power = power * (target:getMod(tpz.mod.PROTECT_EFFECT) / 100)
+  end
+
+   target:delMod(tpz.mod.DEF,power)
 end
