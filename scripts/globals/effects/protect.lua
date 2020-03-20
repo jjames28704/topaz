99450407487d1f15f4e446dfd31c8be065c4cfce
dynisor <dynisor@gmail.com>
2020-03-20 10:05:54 -0700

    Adding in missing PLD gift effects

--- scripts/globals/effects/protect.lua
+++ scripts/globals/effects/protect.lua
@@ -7,12 +7,22 @@ require("scripts/globals/status")
 -----------------------------------
 
 function onEffectGain(target,effect)
-   target:addMod(dsp.mod.DEF,effect:getPower())
+   local power = effect:getPower();
+   if target:hasMod(dsp.mod.PROTECT_EFFECT) then
+      power = power * (target:getMod(dsp.mod.PROTECT_EFFECT) / 100)
+  end
+
+   target:addMod(dsp.mod.DEF,power)
 end
 
 function onEffectTick(target,effect)
 end
 
 function onEffectLose(target,effect)
-   target:delMod(dsp.mod.DEF,effect:getPower())
+   local power = effect:getPower();
+   if target:hasMod(dsp.mod.PROTECT_EFFECT) then
+      power = power * (target:getMod(dsp.mod.PROTECT_EFFECT) / 100)
+  end
+
+   target:delMod(dsp.mod.DEF,power)
 end
--- scripts/globals/spells/reprisal.lua
+++ scripts/globals/spells/reprisal.lua
@@ -13,7 +13,7 @@ end
 function onSpellCast(caster, target, spell)
     local duration = calculateDuration(60, spell:getSkillType(), spell:getSpellGroup(), caster, target)
     local maxReflectedDamage = target:getMaxHP() * 2
-    local reflectedPercent = 33
+    local reflectedPercent = 33 + target:getMod(dsp.mod.REPRISAL_EFFECT)
     local typeEffect = dsp.effect.REPRISAL
 
     if target:addStatusEffect(typeEffect, reflectedPercent, 0, duration, 0, maxReflectedDamage) then
