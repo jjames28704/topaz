787f43a5b0296877a953b7d8fb8e56932027ede5
dynisor <dynisor@gmail.com>
2020-03-20 20:08:37 -0700

    Add VELOCITY_SHOT_DELAY effect

--- scripts/globals/effects/velocity_shot.lua
+++ scripts/globals/effects/velocity_shot.lua
@@ -10,17 +10,17 @@ function onEffectGain(target, effect)
     target:addMod(tpz.mod.ATTP, -15)
     target:addMod(tpz.mod.HASTE_ABILITY, -1500)
     target:addMod(tpz.mod.RATTP, 15)
-    target:addMod(tpz.mod.RANGED_DELAYP, -10)
+    target:addMod(tpz.mod.RANGED_DELAYP, -15 - target:getMod(tpz.mod.VELOCITY_SHOT_DELAY))
 end
 
 function onEffectTick(target, effect)
 end
 
 function onEffectLose(target, effect)
-    local jp_value = target:getJobPointValue(tpz.jp.VELOCITY_SHOT_EFFECT)
+    local jp_value = target:getJobPointValue(dsp.jp.VELOCITY_SHOT_EFFECT)
     target:delMod(tpz.mod.RATT, jp_value * 2)
     target:delMod(tpz.mod.ATTP, -15)
     target:delMod(tpz.mod.HASTE_ABILITY, -1500)
     target:delMod(tpz.mod.RATTP, 15)
-    target:delMod(tpz.mod.RANGED_DELAYP, -10)
+    target:delMod(tpz.mod.RANGED_DELAYP, -15 - target:getMod(tpz.mod.VELOCITY_SHOT_DELAY))
 end
